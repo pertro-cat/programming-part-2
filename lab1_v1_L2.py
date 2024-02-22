b5ff0740667923eb6c9e0782b77a6bc65ac9fc7c
petro <150966625+pertro-cat@users.noreply.github.com>
2024-02-22 19:56:55 +0200

    Update lab1_v1_L2.py

--- lab1_v1_L2.py
+++ lab1_v1_L2.py
@@ -8,6 +8,10 @@ def seeding_plan(m ,n , seeds_in_each_place):
             for j in range(n-1, -1, -1):
                 planting_plan.append(seeds_in_each_place[i][j])
     return planting_plan
-print(seeding_plan(5, 5, [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]))   
+print(seeding_plan(5, 5, [[1, 2, 3, 4, 5], 
+                          [6, 7, 8, 9, 10], 
+                          [11, 12, 13, 14, 15], 
+                          [16, 17, 18, 19, 20], 
+                          [21, 22, 23, 24, 25]]))   
 
-
+ 
