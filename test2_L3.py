b15faf556f1ae947d5e264c3d3ef66078a2128c9
petro <150966625+pertro-cat@users.noreply.github.com>
2024-03-05 12:00:03 +0200

    Update test2_L3.py

--- test2_L3.py
+++ test2_L3.py
@@ -1,30 +1,24 @@
 import unittest
 import sys
-sys.path.append('c:/Visual Python/lab_1/lab_2')
+sys.path.append('c:/Visual Python/lab_1/lab_2/src/')
 from lab2_L3 import max_hamster
 
 
 class TestMaxHamster(unittest.TestCase):
-    def test_example1(self):
-        S = 7
-        C = 3
+    def test_example_1(self):
+        S, C = 7, 3
         hamsters = [[1, 2], [2, 2], [3, 1]]
-        result = max_hamster(S, C, hamsters)
-        self.assertEqual(result, 2)
+        self.assertEqual(max_hamster(S, C, hamsters), 2, "Example 1 failed")
 
-    def test_example2(self):
-        S = 19
-        C = 4
+    def test_example_2(self):
+        S, C = 19, 4
         hamsters = [[5, 0], [2, 2], [1, 4], [5, 1]]
-        result = max_hamster(S, C, hamsters)
-        self.assertEqual(result, 3)
+        self.assertEqual(max_hamster(S, C, hamsters), 3, "Example 2 failed")
 
-    def test_example3(self):
-        S = 2
-        C = 2
+    def test_example_3(self):
+        S, C = 2, 2
         hamsters = [[1, 50000], [1, 60000]]
-        result = max_hamster(S, C, hamsters)
-        self.assertEqual(result, 1)
+        self.assertEqual(max_hamster(S, C, hamsters), 1, "Example 3 failed")
 
 if __name__ == '__main__':
     unittest.main()
