8cba51fb4e2b2fc7848a8a0d0068e5cf309bc7d8
petro <150966625+pertro-cat@users.noreply.github.com>
2024-04-25 20:54:13 +0300

    Update test for new code test_string_fine_automata.py
    оновлений тест до оновленого коду лаби

--- test_string_fine_automata.py
+++ test_string_fine_automata.py
@@ -1,45 +1,40 @@
 import unittest
 import sys
 sys.path.append('c:/Python/PycharmProjects/programming_part2')
-from src.string_finite_automata import search_inText, nextState, automaton_matcher
-NO_OF_CHARS = 256
 
-class TestStringMatching(unittest.TestCase):
-    def test_nextState(self):
-        needle = "AABA"
-        m = len(needle)
-        self.assertEqual(nextState(needle, m, 0, ord('A')), 1)
-        self.assertEqual(nextState(needle, m, 1, ord('A')), 2)
-        self.assertEqual(nextState(needle, m, 2, ord('B')), 3)
-        self.assertEqual(nextState(needle, m, 3, ord('A')), 4)
-        # Test fallback to zero
-        self.assertEqual(nextState(needle, m, 3, ord('C')), 0)
-        self.assertEqual(nextState(needle, m, 2, ord('A')), 1)
+from src.string_finite_automata import transition_table, finite_automaton
 
-    def test_automaton_matcher(self):
-        needle = "ABC"
-        TF = automaton_matcher(needle, len(needle))
-        self.assertEqual(len(TF), 4)  # Including state 0
-        self.assertEqual(TF[0][ord('A')], 1)
-        self.assertEqual(TF[1][ord('B')], 2)
-        self.assertEqual(TF[2][ord('C')], 3)
-        self.assertEqual(TF[3][ord('A')], 0)
+class TestStringFiniteAutomata(unittest.TestCase):
 
-    def test_search_inText(self):
-        needle = "AABA"
+    def test_empty_needle(self):
+        nedle = ""
         haystack = "AABAACAADAABAAABAA"
-        results = search_inText(needle, haystack, len(needle), len(haystack))
-        self.assertEqual(results, [0, 9, 13])
-        needle = "AAC"
-        results = search_inText(needle, haystack, len(needle), len(haystack))
-        self.assertIsNone(results)
-        needle = "LONGNEEDLE"
+        transition_table_result = transition_table(nedle)
+        result = finite_automaton(haystack, nedle, transition_table_result)
+        self.assertEqual(result, -1)
+
+    def test_empty_haystack(self):
+        nedle = "AABA"
+        haystack = ""
+        transition_table_result = transition_table(nedle)
+        result = finite_automaton(haystack, nedle, transition_table_result)
+        self.assertEqual(result, -1)
+
+    def test_needle_larger_than_haystack(self):
+        nedle = "LONGNEEDLE"
         haystack = "SHORT"
-        results = search_inText(needle, haystack, len(needle), len(haystack))
-        self.assertIsNone(results)
+        transition_table_result = transition_table(nedle)
+        result = finite_automaton(haystack, nedle, transition_table_result)
+        self.assertEqual(result, -1)
+
+    def test_all_parameters_correct(self):
+        nedle = "AABA"
+        haystack = "AABAACAADAABAAABAA"
+        transition_table_result = transition_table(nedle)
+        result = finite_automaton(haystack, nedle, transition_table_result)
+        self.assertEqual(result, [0, 9, 13])
 
 if __name__ == '__main__':
     unittest.main()
-
 
 
