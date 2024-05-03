77709a46bde8899c03fa63581fd2ec5cbdee7496
petro <150966625+pertro-cat@users.noreply.github.com>
2024-05-03 16:57:51 +0300

    Update for new code test_network_in_venice.py

--- test_network_in_venice.py
+++ test_network_in_venice.py
@@ -1,23 +1,31 @@
 import unittest
-from src.network_in_venice import Graph, read_input_data
+from src.network_in_venice import floyd_warshall_find_distance, calculate_cable_length, read_csv_file
 
-class TestGraphMethods(unittest.TestCase):
+class TestNetworkInVenice(unittest.TestCase):
 
-    def test_find(self):
-        graph = Graph(5)
-        self.assertEqual(graph.find(0), 0)
+    def test_floyd_warshall_find_distance(self):
+        matrix = [[0, 5, float('inf'), 10],
+                  [float('inf'), 0, 3, float('inf')],
+                  [float('inf'), float('inf'), 0, 1],
+                  [float('inf'), float('inf'), float('inf'), 0]]
+        result = floyd_warshall_find_distance(matrix)
+        expected = [[0, 5, 8, 9],
+                    [float('inf'), 0, 3, 4],
+                    [float('inf'), float('inf'), 0, 1],
+                    [float('inf'), float('inf'), float('inf'), 0]]
+        self.assertEqual(result, expected)
 
-    def test_union(self):
-        graph = Graph(5)
-        graph.union(0, 1)
-        self.assertEqual(graph.find(0), graph.find(1))
+    def test_calculate_cable_length(self):
+        matrix = [[0, 1, 2], [1, 0, 3], [2, 3, 0]]
+        result = calculate_cable_length(matrix)
+        expected = 6.0
+        self.assertEqual(result, expected)
 
-    def test_kruskal(self):
-        edges = [(0, 1, 1), (1, 2, 2), (2, 3, 3), (3, 4, 4)]
-        graph = Graph(5)
-        graph.edges = edges
-        self.assertEqual(graph.kruskal(5), 10)
+    def test_read_csv_file(self):
+
+        result = read_csv_file('islands.csv')
+        expected = [[0, 1, 2], [1, 0, 3], [2, 3, 0]]
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
-
