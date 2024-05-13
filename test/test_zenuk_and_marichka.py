ae220a0aabeadb983f12f32ceb754c0f603bbc1e
petro <150966625+pertro-cat@users.noreply.github.com>
2024-05-13 20:32:48 +0300

    Update and rename lab1_v1_L2_unit.py to test/test_zenuk_and_marichka.py

--- test/test_zenuk_and_marichka.py
+++ test/test_zenuk_and_marichka.py
@@ -1,27 +1,26 @@
 import unittest
-from lab1_v1_L2 import seeding_plan
+from src.zenuk_and_marichka  import seeding_plan
 
 class TestSeedingPlan(unittest.TestCase):
-    def test_5x5(self):
+    def test_5_5(self):
         seeds = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]
         expected_plan = [1, 2, 3, 4, 5, 10, 9, 8, 7, 6, 11, 12, 13, 14, 15, 20, 19, 18, 17, 16, 21, 22, 23, 24, 25]
         self.assertEqual(seeding_plan(5, 5, seeds), expected_plan)
     
-    def test_2x4(self):
+    def test_2_4(self):
         seeds = [[1, 2, 3, 4], [5, 6, 7, 8]]
         expected_plan = [1, 2, 3, 4, 8, 7, 6, 5]
         self.assertEqual(seeding_plan(2, 4, seeds), expected_plan)
     
-    def test_1x6(self):
+    def test_1_6(self):
         seeds = [[1, 2, 3, 4, 5, 6]]
         expected_plan = [1, 2, 3, 4, 5, 6]
         self.assertEqual(seeding_plan(1, 6, seeds), expected_plan)
     
-    def test_6x1(self):
+    def test_6_1(self):
         seeds = [[1], [2], [3], [4], [5], [6]]
         expected_plan = [1, 2, 3, 4, 5, 6]
         self.assertEqual(seeding_plan(6, 1, seeds), expected_plan)
 
 if __name__ == '__main__':
     unittest.main()
-
